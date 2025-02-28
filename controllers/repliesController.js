8458ced07d718845ba656fd26148fcec76588b30
Pedro Marques Esteves <pedromaresteves@gmail.com>
2025-02-28 23:40:06 +0000

    Get full thread

--- controllers/repliesController.js
+++ controllers/repliesController.js
@@ -6,4 +6,8 @@ module.exports = {
         const thread = await databaseQueries.getThreadById(req.body.thread_id);
         return res.redirect(`/b/${thread.board}/${req.body.thread_id}`)
     },
+    getFullThread: async (req, res) => {
+        const thread = await databaseQueries.getThreadById(req.query.thread_id);
+        return res.send(thread)
+    }
 }
--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,9 @@ module.exports = function (app) {
     return threadController.deleteThreadData(req, res, next)
   });;
 
-  app.route('/api/replies/:board').post((req, res, next) => {
+  app.route('/api/replies/:board').get((req, res, next) => {
+    return repliesController.getFullThread(req, res, next);
+  }).post((req, res, next) => {
     return repliesController.addReply(req, res, next);
   });
 
